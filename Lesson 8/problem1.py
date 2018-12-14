2d3154354d940f7dd17f038f7ed666b33ae96d57
shannonay0103 <43756247+shannonay0103@users.noreply.github.com>
2018-12-14 10:07:12 -0500

    Add files via upload

---
+++ Lesson 8/problem1.py
@@ -0,0 +1,19 @@
+import time
+
+age = input('What is your age? ')
+age = int(age)
+print()
+total = ((79 - age) + 2018)
+time.sleep(1)
+print("...thinking...")
+print()
+time.sleep(1)
+print("...thinking...")
+print()
+time.sleep(1)
+print('...thinking...')
+print()
+time.sleep(1)
+
+print('You will die in: ' + str(total))
+total = int(total)
